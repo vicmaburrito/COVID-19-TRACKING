66ea02d24ffec236f8b8c9e6eb24c21c55fd20ac
vicmaburrito <vmaldaraca@gmail.com>
2022-02-03 14:31:21 -0600

    Add test for countries component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar/Navbar';
-import About from './Components/About';
 import CountryDetail from './Components/Countries/Country_detail';
 import './App.css';
 
@@ -13,7 +12,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
         <Route path="/CountryDetail/:id" element={<CountryDetail />} />
       </Routes>
     </BrowserRouter>
